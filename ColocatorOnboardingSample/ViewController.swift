4c80dd472f42f609ed32a04f4b152c899290794e
Robert Tanase <robert@crowdconnected.com>
2020-04-01 12:12:38 +0100

    Initial Commit

---
+++ ColocatorOnboardingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColocatorOnboardingSample
+//
+//  Created by TCode on 01/04/2020.
+//  Copyright © 2020 CrowdConnected. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
